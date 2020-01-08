244d55286e70a80b8671df16082e8ec7a2eddf9d
Jason <ironelder@gmail.com>
2020-01-08 20:04:16 +0900

    1-ListAdapter에 ViewHolder추가
    1-ListAdapter에 ViewHolder추가

--- app/src/main/java/com/ironelder/androidarchitecture/SearchListAdapter.kt
+++ app/src/main/java/com/ironelder/androidarchitecture/SearchListAdapter.kt
@@ -1,4 +1,11 @@
 package com.ironelder.androidarchitecture
 
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
 class SearchListAdapter {
+
+    inner class SearchListItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
 }
