65b3ef3e42d3cefee49c815c817e49a3465aef29
Jason <ironelder@gmail.com>
2020-01-08 20:05:55 +0900

    1-ListView Adapter에 RecyclerViewAdapter 상속
    1-ListView Adapter에 RecyclerViewAdapter 상속

--- app/src/main/java/com/ironelder/androidarchitecture/SearchListAdapter.kt
+++ app/src/main/java/com/ironelder/androidarchitecture/SearchListAdapter.kt
@@ -1,9 +1,19 @@
 package com.ironelder.androidarchitecture
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class SearchListAdapter {
+class SearchListAdapter : RecyclerView.Adapter<SearchListAdapter.SearchListItemViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchListItemViewHolder {
+    }
+
+    override fun getItemCount(): Int {
+    }
+
+    override fun onBindViewHolder(holder: SearchListItemViewHolder, position: Int) {
+    }
 
     inner class SearchListItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
