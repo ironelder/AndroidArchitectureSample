59f35035ef3c6e14b8f1d4806c5f4450df19c03c
Jason <ironelder@gmail.com>
2020-01-08 20:10:09 +0900

    1-ListView Adapter 내부 구현
    1-ListView Adapter 내부 구현

--- app/src/main/java/com/ironelder/androidarchitecture/SearchListAdapter.kt
+++ app/src/main/java/com/ironelder/androidarchitecture/SearchListAdapter.kt
@@ -1,15 +1,25 @@
 package com.ironelder.androidarchitecture
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class SearchListAdapter : RecyclerView.Adapter<SearchListAdapter.SearchListItemViewHolder>(){
+class SearchListAdapter : RecyclerView.Adapter<SearchListAdapter.SearchListItemViewHolder>() {
 
+    private lateinit var mItemList: List<Any>
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchListItemViewHolder {
+        return SearchListItemViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.listitem,
+                parent,
+                false
+            )
+        )
     }
 
     override fun getItemCount(): Int {
+        return mItemList.size
     }
 
     override fun onBindViewHolder(holder: SearchListItemViewHolder, position: Int) {
