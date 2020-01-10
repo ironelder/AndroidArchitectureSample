327b63469f35976391b243c1e3668c438fa00a13
Jason <ironelder@gmail.com>
2020-01-10 12:19:50 +0900

    1-MainActivity에서 RecyclerView사용 할 수 있도록  추가 및 설정 추가
    1-MainActivity에서 RecyclerView사용 할 수 있도록  추가 및 설정 추가

--- app/src/main/java/com/ironelder/androidarchitecture/MainActivity.kt
+++ app/src/main/java/com/ironelder/androidarchitecture/MainActivity.kt
@@ -2,11 +2,19 @@ package com.ironelder.androidarchitecture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rv_searchList.apply {
+            adapter = SearchListAdapter()
+            val linearLayoutManager = LinearLayoutManager(this@MainActivity)
+            linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
+            layoutManager = linearLayoutManager
+        }
     }
 }
