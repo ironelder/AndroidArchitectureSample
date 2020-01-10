f567976f108c3ba1833087d3bbe2ae4c761e5a54
Jason <ironelder@gmail.com>
2020-01-10 12:49:53 +0900

    1-NetworkInterface에 결과값 리턴을 위한 Call 리턴 타입 추가
    1-NetworkInterface에 결과값 리턴을 위한 Call 리턴 타입 추가

--- app/src/main/java/com/ironelder/androidarchitecture/NetworkInterface.kt
+++ app/src/main/java/com/ironelder/androidarchitecture/NetworkInterface.kt
@@ -1,11 +1,12 @@
 package com.ironelder.androidarchitecture
 
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface NetworkInterface {
 
     @GET("v2/search/{type}")
-    fun search(@Path("type") type:String, query:String)
+    fun search(@Path("type") type:String, query:String):Call<Any>
 
 }
