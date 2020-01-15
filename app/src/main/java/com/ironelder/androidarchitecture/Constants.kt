8a63c1dc64dc60c90079e07c5662dcf89e2d1789
Jason <ironelder@gmail.com>
2020-01-15 12:27:43 +0900

    1-network통신을 위한 kakaoAPI 키 추가
    1-network통신을 위한 kakaoAPI 키 추가

--- app/src/main/java/com/ironelder/androidarchitecture/Constants.kt
+++ app/src/main/java/com/ironelder/androidarchitecture/Constants.kt
@@ -1,3 +1,4 @@
 package com.ironelder.androidarchitecture
 
 const val CLIENT_BASE_URL = "https://dapi.kakao.com/"
+const val REST_API_KEY="afbf0e43176d96bb2f20d59e33ff0035"
--- app/src/main/java/com/ironelder/androidarchitecture/NetworkInterface.kt
+++ app/src/main/java/com/ironelder/androidarchitecture/NetworkInterface.kt
@@ -1,12 +1,11 @@
 package com.ironelder.androidarchitecture
 
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Path
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface NetworkInterface {
 
+    @Headers("Authorization: KakaoAK $REST_API_KEY")
     @GET("v2/search/{type}")
     fun search(@Path("type") type:String, @Query("query") query:String):Call<Any>
 
