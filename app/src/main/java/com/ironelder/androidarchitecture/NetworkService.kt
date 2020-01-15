8660c286b31bb599a0287757fe5e80ed54df2cde
Jason <ironelder@gmail.com>
2020-01-15 12:21:13 +0900

    1-데이터 파싱을 위한 컨버터 팩토리 추가
    1-데이터 파싱을 위한 컨버터 팩토리 추가

--- app/src/main/java/com/ironelder/androidarchitecture/NetworkService.kt
+++ app/src/main/java/com/ironelder/androidarchitecture/NetworkService.kt
@@ -1,11 +1,13 @@
 package com.ironelder.androidarchitecture
 
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkService {
     val searchAPI: NetworkInterface by lazy {
         val retrofit = Retrofit.Builder()
             .baseUrl(CLIENT_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
         return@lazy retrofit.create(NetworkInterface::class.java)
     }
