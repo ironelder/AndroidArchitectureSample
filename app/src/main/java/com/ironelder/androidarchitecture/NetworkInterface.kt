990be58d9936f1619a7308731a084c6beaaebadc
Jason <ironelder@gmail.com>
2020-01-15 12:14:50 +0900

    1-NetworkInterface에 Query 어노테이션 추가
    1-NetworkInterface에 Query 어노테이션 추가

--- app/src/main/java/com/ironelder/androidarchitecture/NetworkInterface.kt
+++ app/src/main/java/com/ironelder/androidarchitecture/NetworkInterface.kt
@@ -3,10 +3,11 @@ package com.ironelder.androidarchitecture
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface NetworkInterface {
 
     @GET("v2/search/{type}")
-    fun search(@Path("type") type:String, query:String):Call<Any>
+    fun search(@Path("type") type:String, @Query("query") query:String):Call<Any>
 
 }
