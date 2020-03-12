422df5cd44f050964492c7ac15c608ae1217abd6
Min <minhb@aekyung.kr>
2020-03-12 10:36:42 +0900

    각 검색 종류별로 아이템을 만들도록 함

--- app/src/main/java/com/min/listApp/data/mapper/KakaoSearchEntityMapper.kt
+++ app/src/main/java/com/min/listApp/data/mapper/KakaoSearchEntityMapper.kt
@@ -7,7 +7,7 @@ import com.min.listApp.data.entity.KakaoSearchMetaEntity
 import com.min.listApp.data.model.*
 
 object KakaoSearchEntityMapper {
-    private fun <T : KakaoListItemModel> toImageListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<T> = documents.mapTo(destination = arrayListOf()) {
+    private fun <T : KakaoListItemModel> toImageListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<T> = documents.mapTo(destination = ArrayList()) {
         KakaoImageListItemModel(
             collection = it.collection,
             datetime = it.datetime,
@@ -18,37 +18,39 @@ object KakaoSearchEntityMapper {
             thumbnail_url = it.thumbnail_url,
             width = it.width
         )
-    }
+    } as ArrayList<T>
+
+
 //        documents.mapTo(destination = ArrayList()) {
 //            return@mapTo ArrayList<T>()
 //        }
 
-    private fun toVClipListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<KakaoVclipListItemModel> =
+    private fun <T : KakaoListItemModel> toVClipListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<T> =
         documents.mapTo(destination = ArrayList()) {
             KakaoVclipListItemModel(author = it.author, datetime = it.datetime, play_time = it.play_time, thumbnail = it.thumbnail, title = it.title, url = it.url)
-        }
+        } as ArrayList<T>
 
-    private fun toWebListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<KakaoWebListItemModel> =
+    private fun <T : KakaoListItemModel> toWebListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<T> =
         documents.mapTo(destination = ArrayList()) {
             KakaoWebListItemModel(contents = it.contents, url = it.url, title = it.title, datetime = it.datetime)
-        }
+        } as ArrayList<T>
 
-    private fun toBlogListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<KakaoBlogListItemModel> =
+    private fun <T : KakaoListItemModel> toBlogListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<T> =
         documents.mapTo(destination = ArrayList()) {
             KakaoBlogListItemModel(blogname = it.blogname, datetime = it.datetime, title = it.title, url = it.url, contents = it.contents, thumbnail = it.thumbnail)
-        }
+        } as ArrayList<T>
 
-    private fun toTipListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<KakaoTipListItemModel> =
+    private fun <T : KakaoListItemModel> toTipListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<T> =
         documents.mapTo(destination = ArrayList()) {
             KakaoTipListItemModel(a_url = it.a_url, contents = it.contents, title = it.title, datetime = it.datetime, q_url = it.q_url, thumbnails = it.thumbnails, type = it.type)
-        }
+        } as ArrayList<T>
 
-    private fun toCafeListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<KakaoCafeListItemModel> =
+    private fun <T : KakaoListItemModel> toCafeListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<T> =
         documents.mapTo(destination = ArrayList()) {
             KakaoCafeListItemModel(cafename = it.cafename, url = it.url, thumbnail = it.thumbnail, contents = it.contents, title = it.contents, datetime = it.datetime)
-        }
+        } as ArrayList<T>
 
-    private fun toBookListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<KakaoBookListItemModel> =
+    private fun <T : KakaoListItemModel> toBookListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<T> =
         documents.mapTo(destination = ArrayList()) {
             KakaoBookListItemModel(
                 authors = it.authors,
@@ -64,15 +66,17 @@ object KakaoSearchEntityMapper {
                 status = it.status,
                 translators = it.translators
             )
-        }
+        } as ArrayList<T>
 
-//    private fun toListItem(category: KakaoCategory, documents: List<KakaoSearchDocumentEntity>): ArrayList<KakaoListItemModel> {
-//        when (category) {
-//            KakaoCategory.IMAGE -> return toImageListItem(documents = documents)
-//            KakaoCategory.BLOG -> return toBlogListItem(documents = documents)
-//            KakaoCategory.BLOG -> return toBlogListItem(documents = documents)
-//        }
-//    }
+    private fun toListItem(category: KakaoCategory, documents: List<KakaoSearchDocumentEntity>): ArrayList<KakaoListItemModel> = when (category) {
+        KakaoCategory.IMAGE -> toImageListItem(documents = documents)
+        KakaoCategory.WEB -> toWebListItem(documents = documents)
+        KakaoCategory.BOOK -> toBookListItem(documents = documents)
+        KakaoCategory.CAFE -> toCafeListItem(documents = documents)
+        KakaoCategory.TIP -> toTipListItem(documents = documents)
+        KakaoCategory.VCLIP -> toVClipListItem(documents = documents)
+        KakaoCategory.BLOG -> toBlogListItem(documents = documents)
+    }
 
     private fun toListMeta(meta: KakaoSearchMetaEntity): KakaoListMeta = meta.let {
         KakaoListMeta(
@@ -83,6 +87,6 @@ object KakaoSearchEntityMapper {
     }
 
     fun toListModel(entity: KakaoSearchEntity, category: KakaoCategory): KakaoListModel<KakaoListItemModel> = KakaoListModel(
-        toListMeta(entity.meta), toImageListItem(documents = entity.documents)
+        toListMeta(entity.meta), toListItem(category = category, documents = entity.documents)
     )
 }
