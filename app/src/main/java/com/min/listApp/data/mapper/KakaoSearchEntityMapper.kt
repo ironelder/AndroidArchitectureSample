a073afd2cc11fc210ad9fb256054d2c63f0165a2
Min <1stpie@naver.com>
2020-03-14 16:28:19 +0900

    ArrayList<T> 의 제네릭 타입 비교 경고 뜨지 않도록 변경
    ArrayList<T> 내의 제네릭 타입은 빌드 시점에서 제거되어 ArrayList 로 치환됨. 그렇기에 타입 비교를 할 수 없음. 이를 해결하기 위한 방법이 복잡하여 우선 경고문 제거

--- app/src/main/java/com/min/listApp/data/mapper/KakaoSearchEntityMapper.kt
+++ app/src/main/java/com/min/listApp/data/mapper/KakaoSearchEntityMapper.kt
@@ -7,6 +7,7 @@ import com.min.listApp.data.entity.KakaoSearchMetaEntity
 import com.min.listApp.data.model.*
 
 object KakaoSearchEntityMapper {
+    @Suppress("UNCHECKED_CAST")
     private fun <T : KakaoListItemModel> toImageListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<T> = documents.mapTo(destination = ArrayList()) {
         KakaoImageListItemModel(
             collection = it.collection,
@@ -20,36 +21,37 @@ object KakaoSearchEntityMapper {
         )
     } as ArrayList<T>
 
-
-//        documents.mapTo(destination = ArrayList()) {
-//            return@mapTo ArrayList<T>()
-//        }
-
+    @Suppress("UNCHECKED_CAST")
     private fun <T : KakaoListItemModel> toVClipListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<T> =
         documents.mapTo(destination = ArrayList()) {
             KakaoVclipListItemModel(author = it.author, datetime = it.datetime, play_time = it.play_time, thumbnail = it.thumbnail, title = it.title, url = it.url)
         } as ArrayList<T>
 
+    @Suppress("UNCHECKED_CAST")
     private fun <T : KakaoListItemModel> toWebListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<T> =
         documents.mapTo(destination = ArrayList()) {
             KakaoWebListItemModel(contents = it.contents, url = it.url, title = it.title, datetime = it.datetime)
         } as ArrayList<T>
 
+    @Suppress("UNCHECKED_CAST")
     private fun <T : KakaoListItemModel> toBlogListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<T> =
         documents.mapTo(destination = ArrayList()) {
             KakaoBlogListItemModel(blogname = it.blogname, datetime = it.datetime, title = it.title, url = it.url, contents = it.contents, thumbnail = it.thumbnail)
         } as ArrayList<T>
 
+    @Suppress("UNCHECKED_CAST")
     private fun <T : KakaoListItemModel> toTipListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<T> =
         documents.mapTo(destination = ArrayList()) {
             KakaoTipListItemModel(a_url = it.a_url, contents = it.contents, title = it.title, datetime = it.datetime, q_url = it.q_url, thumbnails = it.thumbnails, type = it.type)
         } as ArrayList<T>
 
+    @Suppress("UNCHECKED_CAST")
     private fun <T : KakaoListItemModel> toCafeListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<T> =
         documents.mapTo(destination = ArrayList()) {
             KakaoCafeListItemModel(cafename = it.cafename, url = it.url, thumbnail = it.thumbnail, contents = it.contents, title = it.contents, datetime = it.datetime)
         } as ArrayList<T>
 
+    @Suppress("UNCHECKED_CAST")
     private fun <T : KakaoListItemModel> toBookListItem(documents: List<KakaoSearchDocumentEntity>): ArrayList<T> =
         documents.mapTo(destination = ArrayList()) {
             KakaoBookListItemModel(
