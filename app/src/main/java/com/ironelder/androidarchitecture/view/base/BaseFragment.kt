7bc6a1b624c072cd96ca598c0eafa5ce4b557214
JangJinHyuk <jhjang@jangboo.kr>
2020-03-19 18:08:04 +0900

    BaseFragment추가
    BaseFragment추가

--- app/src/main/java/com/ironelder/androidarchitecture/view/base/BaseFragment.kt
+++ app/src/main/java/com/ironelder/androidarchitecture/view/base/BaseFragment.kt
@@ -1,4 +1,26 @@
 package com.ironelder.androidarchitecture.view.base
 
-class BaseFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<in VIEW : BaseContract.View, PRESENTER : BaseContract.Presenter<VIEW>, BINDING : ViewDataBinding>(
+    private val resId: Int
+) : Fragment(resId), BaseContract.View {
+    protected lateinit var binding: BINDING
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, resId, container, false)
+        return binding.root
+    }
+
+    abstract val presenter:PRESENTER
 }
