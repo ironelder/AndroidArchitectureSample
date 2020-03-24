f6dcb34ceaffef854b743a1ac74d202a38bf8714
Jason <ironelder@gmail.com>
2020-03-24 12:53:21 +0900

    웹 프래그먼트 코드 정리
    웹 프래그먼트 코드 정리

--- app/src/main/java/com/ironelder/androidarchitecture/view/WebFragment.kt
+++ app/src/main/java/com/ironelder/androidarchitecture/view/WebFragment.kt
@@ -1,9 +1,10 @@
 package com.ironelder.androidarchitecture.view
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
+import android.widget.Toast
 import com.ironelder.androidarchitecture.R
+import com.ironelder.androidarchitecture.common.BLOG_TAB
+import com.ironelder.androidarchitecture.common.showToastMessage
 import com.ironelder.androidarchitecture.component.SearchListAdapter
 import com.ironelder.androidarchitecture.data.model.ListItem
 import com.ironelder.androidarchitecture.databinding.FragmentWebBinding
@@ -14,28 +15,27 @@ class WebFragment :
     BaseFragment<WebContract.View, WebContract.Presenter, FragmentWebBinding>(R.layout.fragment_web),
     WebContract.View {
     override fun onDataChanged(result: List<ListItem>?) {
-        Log.d("ironelderLog", "onDataChanged")
         binding.items = result
     }
 
     override fun showErrorMessage(message: String) {
-        Log.d("ironelderLog", "showErrorMessage")
+        activity?.showToastMessage(message, Toast.LENGTH_SHORT)
     }
 
     override fun showLoading() {
-        Log.d("ironelderLog", "showLoading")
+        binding.isLoading = true
     }
 
     override fun hideLoading() {
-        Log.d("ironelderLog", "hideLoading")
+        binding.isLoading = false
     }
 
     override val presenter = WebPresenter(NetworkUseCase())
 
-    override fun initializedView(savedInstanceState: Bundle?){
+    override fun initializedView(savedInstanceState: Bundle?) {
         with(binding.rvSearchList) {
             adapter = SearchListAdapter()
         }
-        presenter.searchData("blog", "test")
+        presenter.searchData(BLOG_TAB, "test")
     }
 }
