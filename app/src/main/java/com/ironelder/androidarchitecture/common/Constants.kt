91b1f9ee1c95d7f7b1f2d6340750786c4a459d08
Jason <ironelder@gmail.com>
2020-03-24 12:53:58 +0900

    유즈케이스 매퍼 추가 및 상수 정의, 데이터 프로퍼티 추가
    유즈케이스 매퍼 추가 및 상수 정의, 데이터 프로퍼티 추가

--- app/src/main/java/com/ironelder/androidarchitecture/common/Constants.kt
+++ app/src/main/java/com/ironelder/androidarchitecture/common/Constants.kt
@@ -2,4 +2,7 @@ package com.ironelder.androidarchitecture.common
 
 const val CLIENT_BASE_URL = "https://dapi.kakao.com/"
 const val REST_API_KEY="afbf0e43176d96bb2f20d59e33ff0035"
+const val WEB_TAB = "web"
+const val IMAGE_TAB = "image"
 const val BLOG_TAB = "blog"
+const val CAFE_TAB = "cafe"
--- app/src/main/java/com/ironelder/androidarchitecture/data/entity/Document.kt
+++ app/src/main/java/com/ironelder/androidarchitecture/data/entity/Document.kt
@@ -5,6 +5,9 @@ data class Document(
     val datetime: String?,
     val title: String?,
     val thumbnail: String?,
+    val thumbnail_url: String?,
+    val display_sitename: String?,
+    val collection: String?,
     val url: String?,
     val play_time: Int?,
     val author: String?
