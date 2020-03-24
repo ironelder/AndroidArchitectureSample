e9a6c3e14b404d83a99f9fc70ae244d40ef75fc2
Jason <ironelder@gmail.com>
2020-03-24 12:53:09 +0900

    베이스에 disposeBag추가
    베이스에 disposeBag추가

--- app/src/main/java/com/ironelder/androidarchitecture/view/base/BasePresenter.kt
+++ app/src/main/java/com/ironelder/androidarchitecture/view/base/BasePresenter.kt
@@ -1,10 +1,12 @@
 package com.ironelder.androidarchitecture.view.base
 
 import android.util.Log
+import io.reactivex.disposables.CompositeDisposable
 
 abstract class BasePresenter<VIEW : BaseContract.View> : BaseContract.Presenter<VIEW> {
     protected var view: VIEW? = null
         private set
+    protected val disposeBag = CompositeDisposable()
 
     override fun createView(view: VIEW) {
         Log.d("ironelderLog", "BasePresenter createView")
@@ -13,5 +15,6 @@ abstract class BasePresenter<VIEW : BaseContract.View> : BaseContract.Presenter<
 
     override fun destroyView() {
         this.view = null
+        disposeBag.dispose()
     }
 }
