c4a82014bd4c1d78a3ff30c695302ed9a36b888f
Jason <ironelder@gmail.com>
2020-05-10 14:24:09 +0900

    코드 정리 및 클릭리스너 테스트
    코드 정리 및 클릭리스너 테스트

--- app/src/main/java/com/ironelder/androidarchitecture/view/web/WebFragment.kt
+++ app/src/main/java/com/ironelder/androidarchitecture/view/web/WebFragment.kt
@@ -1,19 +1,17 @@
 package com.ironelder.androidarchitecture.view.web
 
 import android.os.Bundle
-import android.util.Log
 import androidx.databinding.Observable
 import com.ironelder.androidarchitecture.R
 import com.ironelder.androidarchitecture.common.WEB_TAB
 import com.ironelder.androidarchitecture.common.showToastMessage
 import com.ironelder.androidarchitecture.component.SearchListAdapter
-import com.ironelder.androidarchitecture.data.model.ListItem
 import com.ironelder.androidarchitecture.databinding.FragmentWebBinding
 import com.ironelder.androidarchitecture.domain.NetworkUseCase
 import com.ironelder.androidarchitecture.view.base.BaseFragment
 
 class WebFragment :
-    BaseFragment<FragmentWebBinding>(R.layout.fragment_web){
+    BaseFragment<FragmentWebBinding>(R.layout.fragment_web) {
 
     override fun initializedView(savedInstanceState: Bundle?) {
         binding.webViewModel = WebViewModel(NetworkUseCase())
@@ -21,10 +19,11 @@ class WebFragment :
             adapter = SearchListAdapter()
         }
         binding.webViewModel?.let {
-            it.searchData(WEB_TAB,"test")
-            it.errorMessage.addOnPropertyChangedCallback(object : Observable.OnPropertyChangedCallback(){
+            it.searchData(WEB_TAB, "test")
+            it.errorMessage.addOnPropertyChangedCallback(object :
+                Observable.OnPropertyChangedCallback() {
                 override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
-                    activity?.showToastMessage(it.errorMessage.get()?:"Error")
+                    activity?.showToastMessage(it.errorMessage.get() ?: "Error")
                 }
             })
         }
@@ -32,6 +31,5 @@ class WebFragment :
 
     override fun onDestroyView() {
         super.onDestroyView()
-        binding.webViewModel.destroyView()
     }
 }
